becf34701669afa92796773de579aa0870faf7f1
Umejr Dzinovic <Umejr.Dzinovic@edu.fh-joanneum.at>
2025-03-13 15:54:11 +0100

    Added simple read() method, which returns a list of each line in data.txt

--- src/main/java/at/fhj/msd/App.java
+++ src/main/java/at/fhj/msd/App.java
@@ -1,10 +1,41 @@
 package at.fhj.msd;
 
-/**
- * Hello world!
- */
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+    String filename = "src/main/resources/data.txt";
+    ArrayList<String> ValidList = read(filename);
+    System.out.println(ValidList);
+
     }
+
+    public static ArrayList<String> read(String filename) {
+
+        ArrayList<String> ValidList = new ArrayList<>();
+        File file = new File(filename);
+
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+
+            String line;
+            while((line = br.readLine()) != null) {
+                ValidList.add(line);
+            }
+            
+        }catch(FileNotFoundException e) {
+            System.out.printf("File '%s' was not found", file);
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+
+        return ValidList;
+
+    }
+
 }
