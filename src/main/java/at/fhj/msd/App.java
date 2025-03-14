dab1785317fdcea69b3c4d0b8af7534a43b82055
Umejr Dzinovic <Umejr.Dzinovic@edu.fh-joanneum.at>
2025-03-14 21:57:33 +0100

    Added attributes as well as simple constructor to Schedule.java

--- src/main/java/at/fhj/msd/App.java
+++ src/main/java/at/fhj/msd/App.java
@@ -8,11 +8,12 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class App {
+
     public static void main(String[] args) {
 
-    String filename = "src/main/resources/data.txt";
-    ArrayList<String> ValidList = read(filename);
-    System.out.println(ValidList);
+        String filename = "src/main/resources/data.txt";
+        ArrayList<String> ValidList = read(filename);
+        System.out.println(ValidList);
 
     }
 
@@ -24,13 +25,13 @@ public class App {
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
 
             String line;
-            while((line = br.readLine()) != null) {
+            while ((line = br.readLine()) != null) {
                 ValidList.add(line);
             }
-            
-        }catch(FileNotFoundException e) {
+
+        } catch (FileNotFoundException e) {
             System.out.printf("File '%s' was not found", file);
-        }catch(IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
---
+++ src/main/java/at/fhj/msd/Schedule.java
@@ -0,0 +1,7 @@
+package at.fhj.msd;
+
+public class Schedule {
+
+    
+    
+}
