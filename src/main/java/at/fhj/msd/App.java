6654b0fc5b4273ed69806c5d3df1db421003da11
Umejr Dzinovic <Umejr.Dzinovic@edu.fh-joanneum.at>
2025-03-14 23:38:03 +0100

    Added new writeData() method --> From teacher

--- src/main/java/at/fhj/msd/App.java
+++ src/main/java/at/fhj/msd/App.java
@@ -15,16 +15,14 @@ public class App {
 
     public static void main(String[] args) {
 
-        String filename = "src/main/resources/data.txt";
+        /*      String filename = "src/main/resources/data.txt";
         ArrayList<String> ValidList = read(filename);
         System.out.println(ValidList);
 
-        System.out.println("");
-
+        System.out.println("");*/
         List<Schedule> schedules = readData("data.txt");
-        for (Schedule schedule: schedules) {
-            System.out.println(schedule.asCsv(","));
-        }
+
+        writeData(schedules, "csv");
 
     }
 
@@ -47,24 +45,55 @@ public class App {
         }
 
         return ValidList;
-
     }
 
     public static List<Schedule> readData(String file) {
         List<Schedule> schedules = new ArrayList<>();
         try {
-        
+
             Path filePath = Paths.get(App.class.getClassLoader().getResource(file).toURI());
             List<String> lines = Files.readAllLines(filePath);
-            for(String line: lines) {
+            for (String line : lines) {
                 String[] parts = line.split(";");
                 Schedule schedule = new Schedule(parts[6], parts[2], parts[1], parts[3], parts[4], parts[7], parts[5]);
                 schedules.add(schedule);
-            }     
+            }
         } catch (Exception e) {
             System.out.println(e);
         }
         return schedules;
     }
+
+    public static void writeData(List<Schedule> data, String type) {
+        List<String> lines = new ArrayList<>();
+
+        String delimiter = null;
+        switch (type) {
+            case "csv":
+                delimiter = ",";
+                break;
+            case "tsv":
+                delimiter = "\t";
+        }
+
+        if (delimiter != null) {
+            for (Schedule schedule : data) {
+                lines.add(schedule.asCsv(delimiter));
+            }
+        } else {
+            for (Schedule schedule : data) {
+                lines.add(schedule.asSql());
+            }
+        }
+
+        try {
+            Files.writeString(Paths.get("data."  + type), String.join("\n", lines.toArray(new String[0]))); //Give empty Array as parameter! The Method should specify the length by itself
+            //The new file will result in root folder. 
+        }catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+
+
 
 }
